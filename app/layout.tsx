50244386a9847fd526c50850f8c0641c3dcbb9d9
Mom1236 <89107436+Mom1236@users.noreply.github.com>
2025-10-14 13:19:20 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,21 +45,6 @@ export default function RootLayout({
           </CartProvider>
         </PasswordGate>
         <Analytics />
-
-        {/* ✅ Tawk.to Script */}
-        <Script id="tawk-to" strategy="afterInteractive">
-          {`
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/68d2e6422528731922c40faa/1j5rt6t78';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        </Script>
       </body>
     </html>
   )
